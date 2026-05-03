83f98ca25fb683122b796761f53b727504bf6269
KASI <kasiram5473@gmail.com>
2026-05-04 03:17:50 +0530

    Deleted all testing files

--- backend/check_db.js
+++
@@ -1,9 +0,0 @@
-const db = require("./config/db");
-db.query("DESCRIBE users;", (err, results) => {
-  if (err) {
-    console.error("Error:", err);
-  } else {
-    console.log("Schema:", results);
-  }
-  process.exit();
-});
